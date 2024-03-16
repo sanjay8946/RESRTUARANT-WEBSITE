cbb4b4946c09258593895a8ac7d4232e0ab38d62
sanjay8946 <161691420+sanjay8946@users.noreply.github.com>
2024-03-16 21:29:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hi");
+let amount=0;
+amount=1000;
+console.log("your bill amount is Rs."+amount);
+const pi=3.14;
+let name = prompt("whats your name")
+console.log('hi',name)
+let a = prompt("enter a number")
+console.log(Number(a)+10)
